80ec52deff753ef9c982a7ce93293bdb36937387
remberluyckx <remberluyckx@gmail.com>
2016-05-06 16:42:33 +0200

    Initial commit

---
+++ Statistics.js
@@ -0,0 +1,26 @@
+$(function()
+{
+    $(".nav .dropdownGender").hover(function(arg) {
+        $("#dropdown-gender").fadeIn("slow");
+    }, function(arg) {
+        $("#dropdown-gender").fadeOut("fast");
+    });
+
+    $(".nav .dropdownAge").hover(function(arg) {
+        $("#dropdown-age").fadeIn("slow");
+    }, function(arg) {
+        $("#dropdown-age").fadeOut("fast");
+    });
+
+    $(".nav .dropdownEthnicity").hover(function(arg) {
+        $("#dropdown-ethnicity").fadeIn("slow");
+    }, function(arg) {
+        $("#dropdown-ethnicity").fadeOut("fast");
+    });
+
+    $(".nav .dropdownCountry").hover(function(arg) {
+        $("#dropdown-country").fadeIn("slow");
+    }, function(arg) {
+        $("#dropdown-country").fadeOut("fast");
+    });
+});
